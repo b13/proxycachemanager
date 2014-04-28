c1f84635cd967c08e8480264653003ef43865945
Benjamin Mack <benni@typo3.org>
2014-04-28 15:47:13 +0200

    [TASK] Only load cache when proxy is configured

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,21 +4,23 @@ if (!defined ('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tx_proxycachemanager'] = unserialize($TYPO3_CONF_VARS['EXT']['extConf']['proxycachemanager']);
-
-
 // register the cache that stores cacheable frontend URLs and their page IDs
-$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['tx_proxy'] = array(
-	'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\StringFrontend',
-	'backend' => 'B13\\Proxycachemanager\\Cache\\Backend\\ReverseProxyCacheBackend',
-	'options' => array(
-		'defaultLifetime' => 0,	// @todo: should be not "infinite" but rather set to whatever the proxy settings are
-		'reverseProxyProvider'  => $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tx_proxycachemanager']['reverseProxyProvider'] ?: 'B13\\Proxycachemanager\\Provider\\CurlHttpProxyProvider',
-		'reverseProxyEndpoints' => $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tx_proxycachemanager']['reverseProxyEndpoints'],
-	),
-	// setting the pages group makes sure that when the page cache is cleared, that this cache is cleared as well
-	'groups' => array('pages', 'proxy')
-);
+// but only if a reverse Proxy IP was set
+if (!empty($TYPO3_CONF_VARS['SYS']['reverseProxyIP'])) {
+	$TYPO3_CONF_VARS['EXTCONF']['tx_proxycachemanager'] = unserialize($TYPO3_CONF_VARS['EXT']['extConf']['proxycachemanager']);
 
-// hook for adding any cacheable frontend URL to our proxy cache
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-cached']['tx_proxycachemanager'] = 'B13\\Proxycachemanager\\Hook\\FrontendHook->addCacheableUrlToProxyCache';
+	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['tx_proxy'] = array(
+		'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\StringFrontend',
+		'backend' => 'B13\\Proxycachemanager\\Cache\\Backend\\ReverseProxyCacheBackend',
+		'options' => array(
+			'defaultLifetime' => 0,	// @todo: should be not "infinite" but rather set to whatever the proxy settings are
+			'reverseProxyProvider'  => $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tx_proxycachemanager']['reverseProxyProvider'] ?: 'B13\\Proxycachemanager\\Provider\\CurlHttpProxyProvider',
+			'reverseProxyEndpoints' => $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tx_proxycachemanager']['reverseProxyEndpoints'],
+		),
+		// setting the pages group makes sure that when the page cache is cleared, that this cache is cleared as well
+		'groups' => array('pages', 'proxy')
+	);
+
+	// hook for adding any cacheable frontend URL to our proxy cache
+	$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-cached']['tx_proxycachemanager'] = 'B13\\Proxycachemanager\\Hook\\FrontendHook->addCacheableUrlToProxyCache';
+}
