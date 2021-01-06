bcedc4cbc94cf29f892a560bdb8dd1129ab92e62
Benni Mack <benni@typo3.org>
2021-01-06 23:55:33 +0100

    [TASK] Add compatibility for guzzle7 and TYPO3 v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'b13 GmbH',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.5-10.9.99',
+            'typo3' => '9.5.5-11.5.99',
         ],
         'conflicts' => [
         ],
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,15 +1,14 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$configuration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('proxycachemanager');
-
-if (empty($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_proxy'])) {
+if (empty($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_proxy'] ?? null)) {
+    $configuration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('proxycachemanager');
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_proxy'] = [
         'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
         'backend' => \B13\Proxycachemanager\Cache\Backend\ReverseProxyCacheBackend::class,
         'options' => [
             'defaultLifetime' => 0, // @todo: should be not "infinite" but rather set to whatever the proxy settings are
-            'reverseProxyProvider' => $configuration['reverseProxyProvider'] ?: \B13\Proxycachemanager\Provider\CurlHttpProxyProvider::class,
+            'reverseProxyProvider' => $configuration['reverseProxyProvider'] ?? \B13\Proxycachemanager\Provider\CurlHttpProxyProvider::class,
             'reverseProxyEndpoints' => $configuration['reverseProxyEndpoints'],
         ],
         // setting the pages group makes sure that when the page cache is cleared, that this cache is cleared as well
